161c9b5c863bfc91dafedfaf42ed173d2934e388
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-09-18 13:51:37 +0900

    Update steel.add.js

--- steel.add.js
+++ steel.add.js
@@ -1,5 +1,6 @@
 
 ;(function(root){
+/*  
 let 
 storyfile=app.opt.main//'story.txt'
 ,to=getTogist(app.opt.u,localStorage.getItem(app.opt.p))
@@ -8,5 +9,10 @@ to.togistsearch(app.id,storyfile).then(d=>{
 root.raw_url=d
 fn.q('.total').onclick=()=>{ window.open(d,'_blank'); }
 })
+*/  
+fn.q('.total').onclick=()=>{ window.open(
+  'https://gnjo.github.io/fatema/index.html?id='+app.id,
+  '_blank' // <- This is what makes it open in a new window.
+);  }  
 
 })(this);
